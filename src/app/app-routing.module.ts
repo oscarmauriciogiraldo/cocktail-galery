4faf06ca0dc3b4dcc00f096692d64a01f0a37177
oscarmauriciogiraldo <oscardan@utp.edu.co>
2023-05-14 00:13:46 -0500

    roting sistem

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CocktailLandingComponent } from './pages/cocktail-landing/cocktail-landing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "The-Cocktail-DB", component: CocktailLandingComponent},
+  {path: "", redirectTo: "/The-Cocktail-DB", pathMatch: "full"},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
